81bc62f1a2b972c0af8a7d0c44a29c9b696506f6
Adil Altynbekov <dlltnbkv@gmail.com>
2026-02-11 14:51:02 +0500

    learn: passing values and references to functions

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
+
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  }
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 2341234234,
+}
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2341234234) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+}
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
